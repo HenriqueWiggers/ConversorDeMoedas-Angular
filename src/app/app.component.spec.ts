0d62508c992664f2bbca49fdd4384f5479971410
henrywigg <henrique.wiggers.99@gmail.com>
2025-12-11 10:35:22 -0300

    Adicionado bootstrap e jquery no projeto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ConversorDeMoedas-Angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ConversorDeMoedas-Angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ConversorDeMoedas-Angular app is running!');
-  });
+ 
 });
